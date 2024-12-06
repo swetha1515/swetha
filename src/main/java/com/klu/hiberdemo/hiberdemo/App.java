7e82dc756ab878e779856d7c57dd7cbc9fa7eca3
PUTTUR MEGHANA <swethaputtur15@gmail.com>
2024-12-07 00:49:15 +0530

    semendexam

---
+++ src/main/java/com/klu/hiberdemo/hiberdemo/App.java
@@ -0,0 +1,13 @@
+package com.klu.hiberdemo.hiberdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
